f12e7e1b70e16d4c27bdd178c316c9b5ea99cc63
REALJUST <REALJUST@users.noreply.github.com>
2026-04-03 17:46:43 +0800

    feat: 见一舟周刊初始化

---
+++ RECENT.md
@@ -0,0 +1,1 @@
+* [第 1 期 - 开篇](https://realjust.github.io/Weekly/posts/01) - 2026-04-03
---
+++ src/config.ts
@@ -0,0 +1,27 @@
+export const SITE = {
+  title: "见一舟",
+  author: "REALJUST",
+  description: "",
+  keywords: "REALJUST,Weekly,Blog",
+  icon: "/icon-144.png",
+  siteImage: "/assets/1.jpg",
+  homePage: "https://realjust.github.io/Weekly",
+  blogPage: "",
+  twitterId: "",
+  githubId: "REALJUST",
+  repo: "REALJUST/Weekly",
+};
+
+export const GISCUS_CONFIG = {
+  repo: "REALJUST/Weekly",
+  repoId: "",
+  category: "General",
+  categoryId: "",
+  mapping: "title",
+  strict: "0",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  theme: "https://gw.alipayobjects.com/os/k/kr/comment.css",
+  lang: "zh-CN",
+};
